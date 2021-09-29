4c66a0712aba64aae62d979e6d486b7703d9dd48
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-29 21:14:13 +0800

    Create UserDaoImpl.java

---
+++ reflect/dao/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package dao;
+
+/**
+ * @author by wyl
+ * @date 2021/9/25.
+ */
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void add() {
+        System.out.println("add");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("delete");
+    }
+}
