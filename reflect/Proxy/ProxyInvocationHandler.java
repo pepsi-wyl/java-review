afac87b73f5c5f139ec8fd978e0d422eb00e59a4
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-29 21:13:27 +0800

    Create ProxyInvocationHandler.java

---
+++ reflect/Proxy/ProxyInvocationHandler.java
@@ -0,0 +1,44 @@
+package Proxy;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author by wyl
+ * @date 2021/9/25.19点19分
+ */
+
+/**
+ * T      接口名称
+ * impl   接口实现类
+ * new ProxyInvocationHandler<UserDao>(new UserDaoImpl()).getProxy();
+ */
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProxyInvocationHandler<T> implements InvocationHandler {
+
+    private Object impl;
+
+    //生成代理类
+    public T getProxy() {
+        return
+                (T)
+                        Proxy.newProxyInstance(
+                                this.getClass().getClassLoader(),
+                                impl.getClass().getInterfaces(),
+                                this);
+    }
+
+    //处理代理类，并返回结果
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return method.invoke(impl, args);
+    }
+
+}
